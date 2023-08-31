7c83bdff6f054ca6e2382006901f2815b5a5391e
Karl <toom.karl42@gmail.com>
2023-08-31 19:43:15 +0300

    completed

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Movie from "./Movie";
-import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 function Row({ title, fetchURL, rowID }) {
   const [movies, setMovies] = useState([]);
@@ -11,26 +11,36 @@ function Row({ title, fetchURL, rowID }) {
     });
   }, [fetchURL]);
 
-
-  function slideLeft(){
-    const slider = document.getElementById('slider' + rowID)
+  function slideLeft() {
+    const slider = document.getElementById("slider" + rowID);
     slider.scrollLeft = slider.scrollLeft - 500;
   }
-  function slideRight(){
-    const slider = document.getElementById('slider' + rowID)
+  function slideRight() {
+    const slider = document.getElementById("slider" + rowID);
     slider.scrollLeft = slider.scrollLeft + 500;
   }
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
       <div className="relative flex items-center group">
-        <MdChevronLeft onClick={slideLeft}  className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" size={40} />
-        <div id={"slider" + rowID} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide">
+        <MdChevronLeft
+          onClick={slideLeft}
+          className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
+        <div
+          id={"slider" + rowID}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide"
+        >
           {movies.map((item, id) => (
-            <Movie key={id} item ={item}/>
+            <Movie key={id} item={item} />
           ))}
         </div>
-        <MdChevronRight onClick={slideRight} className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" size={40}  />
+        <MdChevronRight
+          onClick={slideRight}
+          className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
       </div>
     </>
   );
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import SavedShows from "../components/SavedShows";
 
 function Account() {
   return (
-    <div>Account</div>
-  )
+    <>
+    <div className='w-full text-white'>
+      <img
+        className='w-full h-[400px] object-cover'
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/fb07dbf1-8032-44ea-9e94-2197730d55a9/EE-en-20230828-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+        alt='/'
+      />
+      <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'></div>
+      <div className='absolute top-[20%] p-4 md:p-8'>
+        <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
+      </div>
+    </div>
+    <SavedShows />
+  </>
+  );
 }
 
-export default Account
+export default Account;
+
